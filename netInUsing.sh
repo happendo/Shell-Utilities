8e4c42c278de13e0a32be4f7c28f1ce68d618f6b
Paulo <44546537+happendo@users.noreply.github.com>
2024-06-09 23:54:02 -0300

    Update netInUsing.sh

--- netInUsing.sh
+++ netInUsing.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 LANG=C
-
+# Modifique 'eno1' para a sua interface!
 INTERFACE='eno1'
 MIN_SPEED=5
 
-# Captura a velocidade atual da interface
 CURRENT_SPEED=$(ifstat -i $INTERFACE 1 1 | awk 'NR==3 {print $1}')
 
 # Remove qualquer ponto decimal para comparar inteiros
