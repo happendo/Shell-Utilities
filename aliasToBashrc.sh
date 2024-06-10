7b5117c1c18db9820bfb38d79549c122d48b5829
Paulo <44546537+happendo@users.noreply.github.com>
2024-06-09 23:37:51 -0300

    Update aliasToBashrc.sh

--- aliasToBashrc.sh
+++ aliasToBashrc.sh
@@ -1,4 +1,4 @@
 alias update="sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove && sudo apt clean"
 
-# Esse último alias determina se minha programaçao no crontab resultará no desligamento do meu sistema ou não
+# Esse último alias determina se o script shut.sh presente na minha programaçao do crontab resultará no desligamento do sistema ou não
 alias shutd='if [ -e ~/shut.down ]; then rm ~/shut.down && echo "Sistema não será mais desligado automaticamente"; else touch ~/shut.down && echo "Sistema será desligado automaticamente"; fi'
